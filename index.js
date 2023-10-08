65a417f72093af366e93a5bb8d838884d1b4d6d2
KrammyGod <shi.mark10@gmail.com>
2023-10-08 13:51:17 -0400

    Added a 404 page

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use('/images', (req, res) => {
-    res.status(404).send('Image not found');
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '404.html'));
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
