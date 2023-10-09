67cea7d6e73081a2947a0c7c3451127ce4debdcf
KrammyGod <34082435+KrammyGod@users.noreply.github.com>
2023-10-09 17:27:56 -0400

    Backend improvement (#1)
    * Updated routes and added schema
    
    * Fixed body parser and uploader
    
    * Improved methods and fixed bugs

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const express = require('express');
 const hasher = require('./hasher');
 const path = require('path');
+const pg = require('pg');
 const multer = require('multer');
 
 const PORT = process.env.PORT || 5000;
@@ -12,32 +13,74 @@ const PUBLIC_DIR = 'images';
 // AWS CloudFront URL
 const CDN_URL = 'https://d1irvsiobt1r8d.cloudfront.net';
 
+const pool = new pg.Pool({
+    connectionTimeoutMillis: 2000
+});
+
+/**
+ * Query database safely
+ * @param {string} q The query string
+ * @param {any[]} values Parameters to the query
+ * @returns {Promise<pg.QueryResultRow>} The resulting rows
+ */
+async function query(q, values) {
+    const client = await pool.connect().catch(() => {
+        // Wrap so we can throw our own error.
+        throw new Error('Database connection failed.');
+    });
+    let res = [];
+    try {
+        res = await client.query(q, values).then(res => res.rows);
+    } finally {
+        client.release();
+    }
+    return res;
+};
+
 const app = express();
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, path.join(__dirname, PUBLIC_DIR));
     },
-    filename: (req, file, cb) => {
+    filename: async (req, file, cb) => {
         // Generate a unique filename
         let filename = `${hasher.generateHash(6)}${path.extname(file.originalname)}`;
         let tries = 0;
         while (tries < 10) {
-            if (fs.existsSync(path.join(__dirname, PUBLIC_DIR, filename))) {
+            // Will hit conflict if filename already exists
+            if (await query('INSERT INTO images(fn) VALUES ($1)', [filename]).then(() => false).catch(() => true)) {
                 filename = `${hasher.generateHash(6)}${path.extname(file.originalname)}`;
             } else {
                 break;
             }
             ++tries;
         }
-        if (fs.existsSync(path.join(__dirname, PUBLIC_DIR, filename))) {
+        if (tries === 10) {
             return cb(new Error('Unable to generate unique filename.'));
         }
         cb(null, filename);
     }
 });
 const upload = multer({ storage });
+const bodyParser = multer();
 
 app.use(`/${PUBLIC_DIR}`, express.static(path.join(__dirname, PUBLIC_DIR)));
+
+// Public API returning all sources for any file
+app.use('/source/:filename', (req, res) => {
+    query(
+        'SELECT source FROM images WHERE fn = $1',
+        [req.params.filename]
+    ).then(ret => {
+        // Could be prettier, but for now just plaintext.
+        if (ret.length === 0) {
+            return res.status(404).send('File does not exist.');
+        }
+        res.status(200).send(`Source of image: ${ret[0].source ?? 'unknown'}`);
+    }).catch(() => {
+        res.status(500).send('Unable to get source.');
+    });
+});
 
 /**
  * Authenticates protected API requests.
@@ -57,30 +100,77 @@ function authenticate(req, res, next) {
 app.post('/api/upload', authenticate, (req, res) => {
     upload.array(PUBLIC_DIR)(req, res, e => {
         if (e) console.error(e);
+        const sources = !Array.isArray(req.body?.sources) ? [req.body?.sources] : req.body.sources;
+        req.files.forEach((file, i) => {
+            query(
+                'UPDATE images SET source = $1 WHERE fn = $2',
+                [sources[i], file.filename]
+            ).catch(() => { });
+        });
         res.status(200).send({ urls: req.files.map(file => `${CDN_URL}/${PUBLIC_DIR}/${file.filename}`) });
     });
 });
 
-app.delete('/api/delete', authenticate, (req, res) => {
-    const { filename } = req.query;
-    if (!filename) {
-        return res.status(400).send({ message: 'No filename provided.' });
+// Private API to get all sources for a list of files
+app.post('/api/sources', authenticate, bodyParser.none(), (req, res) => {
+    if (!req.body?.filenames) {
+        return res.status(400).send({ message: 'No filenames provided.' });
     }
-    const filepath = path.join(__dirname, PUBLIC_DIR, filename);
-    if (!fs.existsSync(filepath)) {
-        return res.status(404).send({ message: 'File does not exist.' });
+    const filenames = !Array.isArray(req.body.filenames) ? [req.body.filenames] : req.body.filenames;
+    const sources = [];
+    (async () => {
+        for (const filename of filenames) {
+            const res = await query(
+                'SELECT source FROM images WHERE fn = $1',
+                [filename]
+            ).catch(() => { });
+            sources.push(res?.[0]?.source ?? null);
+        }
+        res.status(200).send({ sources });
+    })();
+});
+
+app.put('/api/update', authenticate, bodyParser.none(), (req, res) => {
+    if (!req.body?.filenames) {
+        return res.status(400).send({ message: 'No filenames provided.' });
     }
-    fs.unlink(filepath, e => {
-        if (e) {
+    const filenames = !Array.isArray(req.body.filenames) ? [req.body.filenames] : req.body.filenames;
+    // We allow sources to be undefined to clear source easily.
+    const sources = !Array.isArray(req.body?.sources) ? [req.body?.sources] : req.body.sources;
+    (async () => {
+        for (const [i, filename] of filenames.entries()) {
+            await query(
+                'UPDATE images SET source = $1 WHERE fn = $2',
+                [sources[i], filename]
+            ).catch(() => { });
+        }
+        res.status(200).send({ message: 'OK' })
+    })();
+});
+
+app.delete('/api/delete', authenticate, bodyParser.none(), (req, res) => {
+    if (!req.body?.filenames) {
+        return res.status(400).send({ message: 'No filenames provided.' });
+    }
+    const filenames = !Array.isArray(req.body.filenames) ? [req.body.filenames] : req.body.filenames;
+    for (const filename of filenames) {
+        const filepath = path.join(__dirname, PUBLIC_DIR, filename);
+        query('DELETE FROM images WHERE fn = $1', [filename]).catch(() => { });
+        if (!fs.existsSync(filepath)) {
+            return res.status(400).send({ message: 'File does not exist.' });
+        }
+        try {
+            fs.unlinkSync(filepath);
+        } catch (e) {
             console.error(e);
             return res.status(500).send({ message: 'Unable to delete file.' });
         }
-        res.status(200).send({ message: 'OK' });
-    });
+    }
+    res.status(200).send({ message: 'OK' });
 });
 
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, '404.html'));
 });
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
---
+++ sql/schema.sql
@@ -0,0 +1,5 @@
+-- Simple schema to support mapping files to source URLs
+CREATE TABLE IF NOT EXISTS images (
+    fn TEXT PRIMARY KEY, -- The filename; includes extension
+    source text
+);
