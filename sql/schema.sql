eb7264517afced543b72a62a122d48d4cd4fbecf
KrammyGod <shi.mark10@gmail.com>
2023-12-10 19:54:17 -0500

    Add count metrics for response codes

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,5 +1,11 @@
 -- Simple schema to support mapping files to source URLs
 CREATE TABLE IF NOT EXISTS images (
     fn TEXT PRIMARY KEY, -- The filename; includes extension
-    source text
+    source TEXT
+);
+
+-- Simple metrics for personal monitoring
+CREATE TABLE IF NOT EXISTS metrics (
+    statusCode INTEGER PRIMARY KEY,
+    count BIGINT NOT NULL DEFAULT 1
 );
