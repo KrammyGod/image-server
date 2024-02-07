3c824feda0d9195f1d6745c8bc6a0d953fb15122
KrammyGod <shi.mark10@gmail.com>
2024-02-06 21:55:13 -0500

    Update max-age of favicon

--- index.js
+++ index.js
@@ -267,7 +267,7 @@ app.delete('/api/delete', authenticate, express.json(), (req, res) => {
     res.status(200).send({ message: `OK, deleted ${successful.join(', ')}` });
 });
 
-app.use('/favicon.ico', express.static(path.join(__dirname, 'favicon.ico')));
+app.use('/favicon.ico', express.static(path.join(__dirname, 'favicon.ico'), { maxAge: '31536000000' }));
 
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, '404.html'));
